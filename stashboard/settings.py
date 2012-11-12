ee10ef993124d3280409d49db5283d36bb62cc5a
Leonardo Meira <lmeira@fiksu.com>
2012-11-12 13:16:24 -0500

    configured settings

--- stashboard/settings.py
+++ stashboard/settings.py
@@ -2,10 +2,10 @@ import os
 
 DEBUG = False
 
-SITE_NAME = "Stashboard"
-SITE_AUTHOR = "Colonel Mustache"
-SITE_URL = "http://stashbooard.appspot.com"
-REPORT_URL = "mailto:help@stashboard.org"
+SITE_NAME = "Status"
+SITE_AUTHOR = "FreeMyApps"
+SITE_URL = "http://status.freemyapps.com/"
+REPORT_URL = "mailto:alex@fiksu.com"
 
 # Twitter update settings
 TWITTER_CONSUMER_KEY = ''
